e92ba19a0b510bc28872e371ef3cfcf0ec16adb4
Tochukwu Nwachukwu <truetochukz@gmail.com>
2022-01-19 18:47:50 +0200

    ASP.NET CoreMVC with Individual Account Authentication

---
+++ ProjectTemplates/CoreMvcPlusAuth/CoreMvcPlusAuth/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CoreMvcPlusAuth.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
